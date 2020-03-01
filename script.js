fd16ba9d1092067619f08062e06fcec87749b033
DonatasI <49387040+DonatasI@users.noreply.github.com>
2020-03-01 13:53:52 +0200

    Atnaujinau Sign in mygtuka ir pridejau kelias lenteles

---
+++ script.js
@@ -0,0 +1,12 @@
+function background1() {
+	document.body.style.backgroundImage  = "url(front_page_PNG.png')";
+}
+function background2() {
+	document.body.style.backgroundImage  = "url('2.jpg')";
+}
+function background3() {
+	document.body.style.backgroundImage  = "url('3.jpg')";
+}
+function background4() {
+	document.body.style.backgroundImage  = "url('4.jpeg')";
+}
