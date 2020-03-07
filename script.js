258e71b355587969299e469d39feb54a6294d5b7
DonatasI <49387040+DonatasI@users.noreply.github.com>
2020-03-07 22:17:02 +0200

    Truputi netvarka, bet kai kas veikia

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ function background3() {
 function background4() {
 	document.body.style.backgroundImage  = "url('4.jpeg')";
 }
+
+
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+  };
+  xmlhttp.open("GET", "maistas.xml", true);
+  xmlhttp.send();
+}
+
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table="<tr><th>Artist</th><th>Title</th></tr>";
+  var x = xmlDoc.getElementsByTagName("CD");
+  for (i = 0; i <x.length; i++) {
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+    "</td></tr>";
+  }
+  document.getElementById("demo").innerHTML = table;
+}
